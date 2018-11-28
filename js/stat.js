fd97913b33ba479b5fc3bf59f3dfd2d70c97a01a
Nikolai <pekarskiy.nikolai@gmail.com>
2018-11-28 11:13:46 +0300

    создание облака и прогресс бара игры

--- js/stat.js
+++ js/stat.js
@@ -1,25 +1,18 @@
 
 //Константы для отрисовки облака.
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 200;
-var CLOUD_X = 100;
-var CLOUD_Y = 50;
+var CLOUD_WIDTH = 300;
+var CLOUD_HEIGHT = 230;
+var CLOUD_X = 40;
+var CLOUD_Y = 200;
+var TEXT_X = 220;//верх низ
+var TEXT_Y = 230;//лево право
+var TEXT_GAP = 35;
 
 var GAP = 10; //расстояние между барами
-var FONT_GAP = 15; //расстояние между текстом
-var TEXT_WIDTH = 50; //расстояние от текста до бара
-var BAR_HEIGHT = 20; //высота бара
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP; //Расчёт ширины бара
-/*
-Если мы будем использовать константы для того, чтобы определить основные параметры отрисовки,
-а остальные параметры расчитывать, нам в будущем будет проще изменять размеры графика, если понадобится.
-
-Например, ширина столбика гистограммы рассчитывается из ширины окна.
-Чтобы понять, какая максимальная длина может быть у столбика, нужно из ширины облака вычесть
-размеры отступов от левого и правого краёв и оставить место для текста.
-*/
-
-
+var FONT_GAP = 20; //расстояние между текстом
+var TEXT_WIDTH = 30; //расстояние от текста до бара
+var barHeight = CLOUD_HEIGHT - GAP - TEXT_WIDTH - GAP; //высота бара
+var BAR_WIDTH = 20;
 
 
 //рисуем облако и тень
@@ -30,38 +23,33 @@ var renderCloud = function(ctx, x, y, color) {
 
 
 window.renderStatistics = function(ctx, players, times) {
-  //передаём цвет облака для функции отрисовки
-  renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, 100, 50, '#fff');
 
   //Отрисовка гистограммы результатов игроков
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_Y + GAP, CLOUD_X + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_Y, CLOUD_X, '#fff');
 
-
-  //Отрисовка первого столбца
   ctx.fillStyle = '#000';
 
+  var playerIndex = 0;
+  var playerName = 'Вы';
 
-  ctx.fillText('Вы', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * 0); //расчёт отступа "+ (GAP + BAR_HEIGHT) * 0" используется только для единообразия
-  ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * 0, barWidth, BAR_HEIGHT); //430 это ширина бара, в переменной barWidth  || (GAP + BAR_HEIGHT) * 0 нужен для единообразия
+  var players = ['Вы', 'Иван', 'Юлия'];
 
-  //Отрисовка второго столбца
+  ctx.fillText('Вы', TEXT_Y + GAP, TEXT_X + GAP + FONT_GAP);  //
+  ctx.fillRect(CLOUD_Y + GAP + TEXT_WIDTH, CLOUD_X + GAP, BAR_WIDTH, barHeight);
 
-  /*
-  Теперь давайте доработаем отрисовку графика так, чтобы каждый из столбцов гистограммы использовал константы,
-  которые мы создали ранее.
-  Для начала попробуем отрисовать подписи к столбикам.
-  Если первый столбец рисуется вверху облака, то последующие нужно сдвигать вниз.
-  Второй столбец гистограммы рисуется аналогично первому, но к его координате нужно
-  добавить высоту ещё одного столбца (BAR_HEIGHT) и ещё один отступ между столбцами (GAP).
-  */
+  playerIndex = 1;
+  playerName = 'Иван';
 
-  ctx.fillText('Иван', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * 1);
-  ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * 1, barWidth, BAR_HEIGHT);
+  ctx.fillText('Иван', TEXT_Y + TEXT_GAP + GAP, TEXT_X + GAP + FONT_GAP);
+  ctx.fillRect(CLOUD_Y + (GAP + TEXT_WIDTH) * 2, CLOUD_X + GAP, BAR_WIDTH, barHeight);
 
-  //Отрисовка 3 столбца
-  ctx.fillText('Юлия', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + GAP + (GAP + BAR_HEIGHT) * 2);
-  ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * 2, barWidth, BAR_HEIGHT);
+  playerIndex = 2;
+  playerName = 'Юлия';
+
+  ctx.fillText('Юлия', TEXT_Y + (TEXT_GAP + GAP) * 2, TEXT_X + GAP + FONT_GAP);
+  ctx.fillRect(CLOUD_Y + (GAP + TEXT_WIDTH) * 3, CLOUD_X + GAP, BAR_WIDTH, barHeight);
+
+
 
 };
