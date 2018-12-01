fb70f762198d3c3d03bd57ead269278dca70d71b
Nikolai <pekarskiy.nikolai@gmail.com>
2018-12-01 16:44:08 +0300

    Ðсделал бары

--- js/stat.js
+++ js/stat.js
@@ -4,6 +4,8 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 40;
 var CLOUD_Y = 200;
+// чтобы отрисовка баров шла вверх
+var INVERSION_BAR_DRAW = 18;
 // верх низ
 var TEXT_X = 220;
 // лево право
@@ -51,8 +53,8 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 450, 55);
-  ctx.fillText('Список результатов:', 450, 75);
+  ctx.fillText('Ура вы победили!', CLOUD_WIDTH, 55);
+  ctx.fillText('Список результатов:', CLOUD_WIDTH, 75);
 
   var maxTime = getMaxElement(times); // вызываем функцию поиска макс. элемента из массива times
 
@@ -63,10 +65,10 @@ window.renderStatistics = function (ctx, names, times) {
     if (names[i] === PLAYER_NAME) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 128, 0.5)';// попытка задать случайный канал
+      ctx.fillStyle = 'rgb(0, 0, 128)';// попытка задать случайный канал
       ctx.globalAlpha = alfaСhannel;
     }
-    ctx.fillRect(TEXT_GAP + CLOUD_Y + (GAP + TEXT_WIDTH) * i, CLOUD_X + GAP, BAR_WIDTH, (MAX_HEIGHT_BAR * times[i]) / maxTime, barHeight);
+    ctx.fillRect(TEXT_GAP + CLOUD_Y + (GAP + TEXT_WIDTH) * i, CLOUD_X + GAP * INVERSION_BAR_DRAW, BAR_WIDTH, (-(MAX_HEIGHT_BAR * times[i]) / maxTime), barHeight);
     if (names.length > 2 && i === 3) {
       ctx.fillText(names[i], TEXT_Y + (GAP * 2) + TEXT_GAP * i, TEXT_X + FONT_GAP + GAP, BAR_WIDTH, BAR_WIDTH);
     } else {
