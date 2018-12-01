301037c06b6c53d5589c9024b2f3ec061b698d8f
Nikolai <pekarskiy.nikolai@gmail.com>
2018-12-01 22:35:46 +0300

    доработка гистоÑграммы

--- js/stat.js
+++ js/stat.js
@@ -1,35 +1,32 @@
 'use strict';
-// Константы для отрисовки облака.
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 40;
-var CLOUD_Y = 200;
-// чтобы отрисовка баров шла вверх
-var INVERSION_BAR_DRAW = 18;
-// верх низ
-var TEXT_X = 220;
-// лево право
-var TEXT_Y = 230;
-var TEXT_GAP = 35;
-// расстояние между барами 10
-var GAP = 10;
-// расстояние между текстом
-var FONT_GAP = 20;
-// расстояние от текста до бара
-var TEXT_WIDTH = 30;
-// высота бара
-var barHeight = CLOUD_HEIGHT - GAP - TEXT_WIDTH - GAP;
-var BAR_WIDTH = 20;
-// максимальная высота для расчёта пропорции
-var MAX_HEIGHT_BAR = 150;
+
+var CLOUD_WIDTH = 420;// размеры облака
+var CLOUD_HEIGHT = 270;// размеры облака
+var CLOUD_X = 100;// начало координат
+var CLOUD_Y = 10;// начало координат
+var CLOUD_COLOR = 'rgba(255, 255, 255, 1)';
+var SHADOW_CLOUD_COLOR = 'rgba(0, 0, 0, 0.7)';
+var INVERSION_BAR_DRAW = 12;// чтобы отрисовка баров шла вверх
+var TEXT_X = 120;// Имена игроков. лево право. начало координат
+var TEXT_Y = 230;// Имена игроков. лево право. начало координат
+var TEXT_GAP = 50;// Расстояние от текста
+var GAP = 10;// расстояние между барами 10
+var FONT_GAP = 20;// расстояние между текстом до бара
+// var TEXT_WIDTH = 20;// Размер шрифта + отступы
+// var RESTRICTION_HEIGHT_BAR = 80;// для расчёта максимальной
+var maxHeightBar = CLOUD_HEIGHT / 1.8;// максимальная высота для расчёта пропорции
+var barHeight = maxHeightBar;// высота бара
+var BAR_WIDTH = 40;
 var PLAYER_NAME = 'Вы';
+var MY_COLOR = 'rgba(255, 0, 0, 1)';
+var ANY_PLAYER_COLOR = 'rgba(0,0,102,1)';
+
 // рисуем облако и тень
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-
-// нахождение максимума в массиве
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0]; // максимальный элемент
@@ -41,38 +38,64 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var maxTime = getMaxElement(times);// вызываем функцию поиска макс. элемента из массива times
+
 var getRandom = function () {
   return Math.random();
 };
 
-window.renderStatistics = function (ctx, names, times) {
+var getRenderMyBar = function (ctx, names, times) {
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = MY_COLOR;
+    ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
+    ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
+  }
+};
 
-  // Отрисовка гистограммы результатов игроков
-  renderCloud(ctx, CLOUD_Y + GAP, CLOUD_X + GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_Y, CLOUD_X, '#fff');
+var getRenderOtherBar = function (ctx, names, times) {
+  for (var i = 0; i < names.length; i++) {
+    var alfaСhannel = getRandom();
+    ctx.fillStyle = ANY_PLAYER_COLOR;// попытка задать случайный канал
+    ctx.globalAlpha = alfaСhannel;
+    ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
+    ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
+  }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_CLOUD_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_WIDTH, 55);
-  ctx.fillText('Список результатов:', CLOUD_WIDTH, 75);
+  ctx.fillText('Ура вы победили!', TEXT_GAP + CLOUD_X, 35);
+  ctx.fillText('Список результатов:', TEXT_GAP + CLOUD_X, 55);
 
-  var maxTime = getMaxElement(times); // вызываем функцию поиска макс. элемента из массива times
-
+  // var maxTime = getMaxElement(times);// вызываем функцию поиска макс. элемента из массива times
 
   for (var i = 0; i < names.length; i++) {
-    // без этого условия, последний текстовый элемент почему-то сильно смещался.
-    var alfaСhannel = getRandom();
     if (names[i] === PLAYER_NAME) {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      getRenderMyBar(ctx, names, times);
     } else {
-      ctx.fillStyle = 'rgb(0, 0, 128)';// попытка задать случайный канал
-      ctx.globalAlpha = alfaСhannel;
-    }
-    ctx.fillRect(TEXT_GAP + CLOUD_Y + (GAP + TEXT_WIDTH) * i, CLOUD_X + GAP * INVERSION_BAR_DRAW, BAR_WIDTH, (-(MAX_HEIGHT_BAR * times[i]) / maxTime), barHeight);
-    if (names.length > 2 && i === 3) {
-      ctx.fillText(names[i], TEXT_Y + (GAP * 2) + TEXT_GAP * i, TEXT_X + FONT_GAP + GAP, BAR_WIDTH, BAR_WIDTH);
-    } else {
-      ctx.fillText(names[i], TEXT_Y + GAP + TEXT_GAP * i, TEXT_X + FONT_GAP + GAP, BAR_WIDTH, BAR_WIDTH);
+      getRenderOtherBar(ctx, names, times);
     }
   }
 };
+
+// Это как у меня было после доработок и добавления дополнительных констант
+
+/*
+for (var i = 0; i < names.length; i++) {
+  if (names[i] === PLAYER_NAME) {
+    ctx.fillStyle = MY_COLOR;
+    ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
+    ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
+  } else {
+    var alfaСhannel = getRandom();
+    ctx.fillStyle = ANY_PLAYER_COLOR;// попытка задать случайный канал
+    ctx.globalAlpha = alfaСhannel;
+    ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
+    ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
+  }
+}
+*/
