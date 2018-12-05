2c174cf87ca54cb811f36d3aa66b40e0d8d142cd
Nikolai <pekarskiy.nikolai@gmail.com>
2018-12-06 00:48:09 +0300

    статистика полностью готоа

--- js/stat.js
+++ js/stat.js
@@ -12,14 +12,11 @@ var TEXT_Y = 230;// Имена игроков. лево право. начало
 var TEXT_GAP = 50;// Расстояние от текста
 var GAP = 10;// расстояние между барами 10
 var FONT_GAP = 20;// расстояние между текстом до бара
-// var TEXT_WIDTH = 20;// Размер шрифта + отступы
-// var RESTRICTION_HEIGHT_BAR = 80;// для расчёта максимальной
 var maxHeightBar = CLOUD_HEIGHT / 1.8;// максимальная высота для расчёта пропорции
 var barHeight = maxHeightBar;// высота бара
 var BAR_WIDTH = 40;
 var PLAYER_NAME = 'Вы';
 var MY_COLOR = 'rgba(255, 0, 0, 1)';
-
 
 // рисуем облако и тень
 
@@ -51,47 +48,19 @@ var getRandomColor = function () {
   return 'rgba(' + rColor + ',' + gColor + ',' + bColor + ',' + aColor + ')';
 };
 
-/*
-var getRandom = function () {
-  return Math.random();
-};
-*/
-
-var getRenderMyBar = function (ctx, names, times) {
+var getRenderOtherBar = function (ctx, names, times) {
   var maxTime = getMaxElement(times);// вызываем функцию поиска макс. элемента из массива times
   for (var i = 0; i < names.length; i++) {
-    if (names === 'Вы') {
+    if (names[i] === PLAYER_NAME) { // должно найти элемент массива который неравен текущему игроку
       ctx.fillStyle = MY_COLOR;
       ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
       ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
+    } else {
+    // Вызов функции для заливки. Задаём определённый цвет
+      ctx.fillStyle = getRandomColor();
+      ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
+      ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
     }
-  }
-};
-
-var getRenderOtherBar = function (ctx, names, times) {
-  var maxTime = getMaxElement(times);// вызываем функцию поиска макс. элемента из массива times
-
-  /*
-  var count = 0;
-  for (var i = 0; i < names.length; i += 1) {
-    if (i in names) {
-      count += 1;
-    }
-   }
-  for (var j = 0; j < names.length; j++) {
-    if (names[j] === 'Вы') {
-    names.splice([j], 1);
-    }
-  }
- */
-
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') { // должно найти элемент массива который равен текущему игроку
-      names.splice([i], 1); // Удалем этот элемент массива
-    }
-    ctx.fillStyle = getRandomColor();// Вызов функции для заливки. Задаём случайный цвет
-    ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
-    ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
   }
 };
 
@@ -104,31 +73,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', TEXT_GAP + CLOUD_X, 35);
   ctx.fillText('Список результатов:', TEXT_GAP + CLOUD_X, 55);
 
-  // var maxTime = getMaxElement(times);// вызываем функцию поиска макс. элемента из массива times
-
   for (var i = 0; i < names.length; i++) {
-    if (names[i] === PLAYER_NAME) {
-      getRenderMyBar(ctx, names, times);
-    } else {
-      getRenderOtherBar(ctx, names, times);
-    }
+    getRenderOtherBar(ctx, names, times);
   }
 };
-
-// Это как у меня было после доработок и добавления дополнительных констант
-
-/*
-for (var i = 0; i < names.length; i++) {
-  if (names[i] === PLAYER_NAME) {
-    ctx.fillStyle = MY_COLOR;
-    ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
-    ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
-  } else {
-    var alfaСhannel = getRandom();
-    ctx.fillStyle = ANY_PLAYER_COLOR;// попытка задать случайный канал
-    ctx.globalAlpha = alfaСhannel;
-    ctx.fillRect(TEXT_X + GAP + TEXT_GAP * i, (CLOUD_Y + GAP) * INVERSION_BAR_DRAW, BAR_WIDTH, (-(maxHeightBar * times[i]) / maxTime), barHeight);
-    ctx.fillText(names[i], TEXT_X + GAP + TEXT_GAP * i, TEXT_Y + FONT_GAP + GAP, BAR_WIDTH);
-  }
-}
-*/
